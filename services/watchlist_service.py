9ceff1c0c357a56be78c1c0af6fc030a77ecdc04
Aurelis <wzltmp@gmail.com>
2026-07-08 19:28:05 -0700

    fix: sort watchlist by date added

--- services/watchlist_service.py
+++ services/watchlist_service.py
@@ -45,7 +45,7 @@ def add_to_watchlist(user_id, film_id):
 
 def get_watchlist(user_id):
     """
-    Return all films on a user's watchlist.
+    Return all films on a user's watchlist, sorted newest first.
 
     Args:
         user_id (str): UUID of the user.
@@ -56,8 +56,7 @@ def get_watchlist(user_id):
     entries = (
         WatchlistEntry.query
         .filter_by(user_id=user_id)
-        .join(Film)
-        .order_by(Film.title.asc())
+        .order_by(WatchlistEntry.date_added.desc())
         .all()
     )
 
